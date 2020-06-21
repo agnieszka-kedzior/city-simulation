d1175afc5c45fbf3de109cb4e87f52bce9e298fa
ADMIN <agnieszka.czerwonka@student.wat.edu.pl>
2020-06-21 21:44:58 +0200

    + statystyka

---
+++ src/ieee1516e/StatystykaFED/StatystykaFederat.java
@@ -0,0 +1,249 @@
+package ieee1516e.StatystykaFED;
+
+import hla.rti1516e.*;
+import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.encoding.HLAASCIIstring;
+import hla.rti1516e.exceptions.FederatesCurrentlyJoined;
+import hla.rti1516e.exceptions.FederationExecutionAlreadyExists;
+import hla.rti1516e.exceptions.FederationExecutionDoesNotExist;
+import hla.rti1516e.exceptions.RTIexception;
+import hla.rti1516e.time.HLAfloat64Interval;
+import hla.rti1516e.time.HLAfloat64Time;
+import hla.rti1516e.time.HLAfloat64TimeFactory;
+import ieee1516e.StanSwiatel;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class StatystykaFederat {
+    //----------------------------------------------------------
+    //                    STATIC VARIABLES
+    //----------------------------------------------------------
+    public static final String READY_TO_RUN = "ReadyToRun";
+
+    //----------------------------------------------------------
+    //                   INSTANCE VARIABLES
+    //----------------------------------------------------------
+    private RTIambassador rtiamb;
+    private StatystykaFederatAmbassador fedamb;  // created when we connect
+    private HLAfloat64TimeFactory timeFactory; // set when we join
+    protected EncoderFactory encoderFactory;     // set when we join
+
+    protected InteractionClassHandle zmianaSwiatelHandle;
+    protected ParameterHandle stanSwiatelHandle;
+
+    protected ObjectClassHandle mostHandle;
+    protected AttributeHandle mostKierunekHandle;
+    protected HLAASCIIstring aktulanyStanMostu;
+
+    //----------------------------------------------------------
+    //                      CONSTRUCTORS
+    //----------------------------------------------------------
+
+    //----------------------------------------------------------
+    //                    INSTANCE METHODS
+    //----------------------------------------------------------
+
+    private void log(String message) {
+        System.out.println("StatystykaFederate   : " + message);
+    }
+
+    private void waitForUser() {
+        log(" >>>>>>>>>> Press Enter to Continue <<<<<<<<<<");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            reader.readLine();
+        } catch (Exception e) {
+            log("Error while waiting for user input: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////// Main Simulation Method /////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    public void runFederate(String federateName) throws Exception {
+        /////////////////////////////////////////////////
+        // 1 & 2. create the RTIambassador and Connect //
+        /////////////////////////////////////////////////
+        log("Creating RTIambassador");
+        rtiamb = RtiFactoryFactory.getRtiFactory().getRtiAmbassador();
+        encoderFactory = RtiFactoryFactory.getRtiFactory().getEncoderFactory();
+
+        // connect
+        log("Connecting...");
+        fedamb = new StatystykaFederatAmbassador(this);
+        rtiamb.connect(fedamb, CallbackModel.HLA_EVOKED);
+
+        //////////////////////////////
+        // 3. create the federation //
+        //////////////////////////////
+        log("Creating Federation...");
+        // We attempt to create a new federation with the first three of the
+        // restaurant FOM modules covering processes, food and drink
+        try {
+            URL[] modules = new URL[]{
+                    (new File("foms/HLA.xml")).toURI().toURL(),
+            };
+
+            rtiamb.createFederationExecution("Federation", modules);
+            log("Created Federation");
+        } catch (FederationExecutionAlreadyExists exists) {
+            log("Didn't create federation, it already existed");
+        } catch (MalformedURLException urle) {
+            log("Exception loading one of the FOM modules from disk: " + urle.getMessage());
+            urle.printStackTrace();
+            return;
+        }
+
+        URL[] joinModules = new URL[]{
+                (new File("foms/HLA.xml")).toURI().toURL()
+        };
+
+        rtiamb.joinFederationExecution(federateName,
+                "FederateType",
+                "Federation",
+                joinModules);
+
+        log("Joined Federation as " + federateName);
+
+        this.timeFactory = (HLAfloat64TimeFactory) rtiamb.getTimeFactory();
+
+
+        rtiamb.registerFederationSynchronizationPoint(READY_TO_RUN, null);
+        // wait until the point is announced
+        while (fedamb.isAnnounced == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+
+        waitForUser();
+
+        rtiamb.synchronizationPointAchieved(READY_TO_RUN);
+        log("Achieved sync point: " + READY_TO_RUN + ", waiting for federation...");
+        while (fedamb.isReadyToRun == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+
+        enableTimePolicy();
+        log("Time Policy Enabled");
+
+        aktulanyStanMostu = this.encoderFactory.createHLAASCIIstring();
+
+        subscribeMost();
+        subscribeSwiatla();
+        log("Published and Subscribed");
+
+        while (fedamb.isRunning) {
+            // 9.1 update the attribute values of the instance //
+
+            // 9.2 send an interaction
+
+            // 9.3 request a time advance and wait until we get it
+            advanceTime(1.0);
+            log("Time Advanced to " + fedamb.federateTime);
+        }
+
+        rtiamb.resignFederationExecution(ResignAction.DELETE_OBJECTS);
+        log("Resigned from Federation");
+
+        try {
+            rtiamb.destroyFederationExecution("StatystykaFederation");
+            log("Destroyed Federation");
+        } catch (FederationExecutionDoesNotExist dne) {
+            log("No need to destroy federation, it doesn't exist");
+        } catch (FederatesCurrentlyJoined fcj) {
+            log("Didn't destroy federation, federates still joined");
+        }
+    }
+
+    private void enableTimePolicy() throws Exception {
+        // NOTE: Unfortunately, the LogicalTime/LogicalTimeInterval create code is
+        //       Portico specific. You will have to alter this if you move to a
+        //       different RTI implementation. As such, we've isolated it into a
+        //       method so that any change only needs to happen in a couple of spots
+        HLAfloat64Interval lookahead = timeFactory.makeInterval(fedamb.federateLookahead);
+
+        ////////////////////////////
+        // enable time regulation //
+        ////////////////////////////
+        this.rtiamb.enableTimeRegulation(lookahead);
+
+        // tick until we get the callback
+        while (fedamb.isRegulating == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+
+        /////////////////////////////
+        // enable time constrained //
+        /////////////////////////////
+        this.rtiamb.enableTimeConstrained();
+
+        // tick until we get the callback
+        while (fedamb.isConstrained == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+    }
+
+    private void subscribeMost() throws RTIexception {
+        this.mostHandle = rtiamb.getObjectClassHandle("HLAobjectRoot.Most");
+        this.mostKierunekHandle = rtiamb.getAttributeHandle(mostHandle,"kierunek");
+
+        AttributeHandleSet mostAttributes = rtiamb.getAttributeHandleSetFactory().create();
+        mostAttributes.add(mostKierunekHandle);
+
+        rtiamb.subscribeObjectClassAttributes(mostHandle, mostAttributes);
+        log("Most Subscription Set");
+    }
+
+    private void subscribeSwiatla() throws RTIexception {
+        this.zmianaSwiatelHandle = rtiamb.getInteractionClassHandle("HLAinteractionRoot.zmianaSwiatel");
+        this.stanSwiatelHandle = rtiamb.getParameterHandle(zmianaSwiatelHandle, "stanSwiatel");
+
+        rtiamb.subscribeInteractionClass(zmianaSwiatelHandle);
+    }
+
+    private void advanceTime(double timestep) throws RTIexception {
+        // request the advance
+        fedamb.isAdvancing = true;
+        HLAfloat64Time time = timeFactory.makeTime(fedamb.federateTime + timestep);
+        rtiamb.timeAdvanceRequest(time);
+
+        // wait for the time advance to be granted. ticking will tell the
+        // LRC to start delivering callbacks to the federate
+        while (fedamb.isAdvancing) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+    }
+
+    private void deleteObject(ObjectInstanceHandle handle) throws RTIexception {
+        rtiamb.deleteObjectInstance(handle, generateTag());
+    }
+
+    private short getTimeAsShort() {
+        return (short) fedamb.federateTime;
+    }
+
+    private byte[] generateTag() {
+        return ("(timestamp) " + System.currentTimeMillis()).getBytes();
+    }
+
+    //----------------------------------------------------------
+    //                     STATIC METHODS
+    //----------------------------------------------------------
+    public static void main(String[] args) {
+        String federateName = "statystykaFederate";
+        if (args.length != 0) {
+            federateName = args[0];
+        }
+
+        try {
+            new StatystykaFederat().runFederate(federateName);
+        } catch (Exception rtie) {
+            rtie.printStackTrace();
+        }
+    }
+}
---
+++ src/ieee1516e/StatystykaFED/StatystykaFederatAmbassador.java
@@ -0,0 +1,236 @@
+package ieee1516e.StatystykaFED;
+
+
+import hla.rti1516e.*;
+import hla.rti1516e.encoding.DecoderException;
+import hla.rti1516e.encoding.HLAASCIIstring;
+import hla.rti1516e.exceptions.FederateInternalError;
+import hla.rti1516e.time.HLAfloat64Time;
+
+
+public class StatystykaFederatAmbassador extends NullFederateAmbassador {
+    //----------------------------------------------------------
+    //                    STATIC VARIABLES
+    //----------------------------------------------------------
+
+    //----------------------------------------------------------
+    //                   INSTANCE VARIABLES
+    //----------------------------------------------------------
+    private StatystykaFederat federate;
+
+    // these variables are accessible in the package
+    protected double federateTime = 0.0;
+    protected double federateLookahead = 1.0;
+
+    protected boolean isRegulating = false;
+    protected boolean isConstrained = false;
+    protected boolean isAdvancing = false;
+
+    protected boolean isAnnounced = false;
+    protected boolean isReadyToRun = false;
+
+    protected boolean isRunning = true;
+    //----------------------------------------------------------
+    //                      CONSTRUCTORS
+    //----------------------------------------------------------
+
+    public StatystykaFederatAmbassador(StatystykaFederat federate) {
+        this.federate = federate;
+    }
+
+    //----------------------------------------------------------
+    //                    INSTANCE METHODS
+    //----------------------------------------------------------
+    private void log(String message) {
+        System.out.println("FederateAmbassador: " + message);
+    }
+
+    protected ObjectInstanceHandle mostHandle = new ObjectInstanceHandle() {
+        @Override
+        public int encodedLength() {
+            return 0;
+        }
+
+        @Override
+        public void encode(byte[] bytes, int i) {
+
+        }
+    };
+    //////////////////////////////////////////////////////////////////////////
+    ////////////////////////// RTI Callback Methods //////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+    @Override
+    public void synchronizationPointRegistrationFailed(String label,
+                                                       SynchronizationPointFailureReason reason) {
+        log("Failed to register sync point: " + label + ", reason=" + reason);
+    }
+
+    @Override
+    public void synchronizationPointRegistrationSucceeded(String label) {
+        log("Successfully registered sync point: " + label);
+    }
+
+    @Override
+    public void announceSynchronizationPoint(String label, byte[] tag) {
+        log("Synchronization point announced: " + label);
+        if (label.equals(StatystykaFederat.READY_TO_RUN))
+            this.isAnnounced = true;
+    }
+
+    @Override
+    public void federationSynchronized(String label, FederateHandleSet failed) {
+        log("Federation Synchronized: " + label);
+        if (label.equals(StatystykaFederat.READY_TO_RUN))
+            this.isReadyToRun = true;
+    }
+
+    /**
+     * The RTI has informed us that time regulation is now enabled.
+     */
+    @Override
+    public void timeRegulationEnabled(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isRegulating = true;
+    }
+
+    @Override
+    public void timeConstrainedEnabled(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isConstrained = true;
+    }
+
+    @Override
+    public void timeAdvanceGrant(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isAdvancing = false;
+    }
+
+    @Override
+    public void discoverObjectInstance(ObjectInstanceHandle theObject,
+                                       ObjectClassHandle theObjectClass,
+                                       String objectName)
+            throws FederateInternalError {
+        if(theObjectClass.equals(federate.mostHandle)) mostHandle = theObject;
+
+        log("Discoverd Object: handle=" + theObject + ", classHandle=" +
+                theObjectClass + ", name=" + objectName);
+    }
+
+    @Override
+    public void reflectAttributeValues(ObjectInstanceHandle theObject,
+                                       AttributeHandleValueMap theAttributes,
+                                       byte[] tag,
+                                       OrderType sentOrder,
+                                       TransportationTypeHandle transport,
+                                       SupplementalReflectInfo reflectInfo)
+            throws FederateInternalError {
+        // just pass it on to the other method for printing purposes
+        // passing null as the time will let the other method know it
+        // it from us, not from the RTI
+        reflectAttributeValues(theObject,
+                theAttributes,
+                tag,
+                sentOrder,
+                transport,
+                null,
+                sentOrder,
+                reflectInfo);
+    }
+
+    @Override
+    public void reflectAttributeValues(ObjectInstanceHandle theObject,
+                                       AttributeHandleValueMap theAttributes,
+                                       byte[] tag,
+                                       OrderType sentOrdering,
+                                       TransportationTypeHandle theTransport,
+                                       LogicalTime time,
+                                       OrderType receivedOrdering,
+                                       SupplementalReflectInfo reflectInfo)
+            throws FederateInternalError {
+
+        StringBuilder builder = new StringBuilder( "Reflection for object:" );
+
+        builder.append( " handle=" + theObject );
+        /*
+        builder.append( ", tag=" + new String(tag) );
+        if( time != null )
+        {
+            builder.append( ", time=" + ((HLAfloat64Time)time).getValue() );
+        }*/
+
+        builder.append( ", attributeCount=" + theAttributes.size() );
+
+        HLAASCIIstring stanMostu = federate.encoderFactory.createHLAASCIIstring();
+
+        for( AttributeHandle attributeHandle : theAttributes.keySet() )
+        {
+            builder.append( "attributeHandle=" );
+
+            if(attributeHandle.equals(federate.mostKierunekHandle)){
+                builder.append( attributeHandle );
+                builder.append( " (Most)" );
+                try {
+                    stanMostu.decode(theAttributes.get(attributeHandle));
+                    builder.append( " stan: " + stanMostu.getValue());
+                } catch (DecoderException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                builder.append( attributeHandle );
+                builder.append( " (Unknown)   " );
+            }
+        }
+
+        federate.aktulanyStanMostu.setValue(stanMostu.getValue());
+
+        log( builder.toString() );
+    }
+
+    @Override
+    public void receiveInteraction(InteractionClassHandle interactionClass,
+                                   ParameterHandleValueMap theParameters,
+                                   byte[] tag,
+                                   OrderType sentOrdering,
+                                   TransportationTypeHandle theTransport,
+                                   SupplementalReceiveInfo receiveInfo)
+            throws FederateInternalError {
+        // just pass it on to the other method for printing purposes
+        // passing null as the time will let the other method know it
+        // it from us, not from the RTI
+        this.receiveInteraction(interactionClass,
+                theParameters,
+                tag,
+                sentOrdering,
+                theTransport,
+                null,
+                sentOrdering,
+                receiveInfo);
+    }
+
+    @Override
+    public void receiveInteraction(InteractionClassHandle interactionClass,
+                                   ParameterHandleValueMap theParameters,
+                                   byte[] tag,
+                                   OrderType sentOrdering,
+                                   TransportationTypeHandle theTransport,
+                                   LogicalTime time,
+                                   OrderType receivedOrdering,
+                                   SupplementalReceiveInfo receiveInfo)
+            throws FederateInternalError {
+    }
+
+    @Override
+    public void removeObjectInstance(ObjectInstanceHandle theObject,
+                                     byte[] tag,
+                                     OrderType sentOrdering,
+                                     SupplementalRemoveInfo removeInfo)
+            throws FederateInternalError {
+        log("Object Removed: handle=" + theObject);
+    }
+
+    //----------------------------------------------------------
+    //                     STATIC METHODS
+    //----------------------------------------------------------
+}
